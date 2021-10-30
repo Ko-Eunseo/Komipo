1b5637cac03d52fd617a39a9cc7c1a8086e928dc
github Ko-Eunseo <github koyk0408@naver.com>
2021-10-30 22:04:55 +0900

    New project

---
+++ js/common.js
@@ -0,0 +1,15 @@
+
+// gnb open and close
+function openNcloseNav(){
+  var clickbtn = document.getElementById("gnbBox");
+  if(clickbtn.style.display === "none"){
+      clickbtn.style.display = "block";
+  }else{
+      clickbtn.style.display = "none";
+  }
+}
+
+// open menu in gnb
+function openMenu() {
+
+}
