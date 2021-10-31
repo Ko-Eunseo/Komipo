1729f6bd06205a019c1f8966e96e45c0f8a9ee73
github Ko-Eunseo <github koyk0408@naver.com>
2021-10-31 20:18:58 +0900

    Komipo service slide

--- js/common.js
+++ js/common.js
@@ -9,7 +9,19 @@ function openNcloseNav(){
   }
 }
 
-// open menu in gnb
-function openMenu() {
-
-}
+// button
+document.querySelector('.btn1').addEventListener('click', function(){
+  document.querySelector('.container').style.transform = 'translate(0)';
+});
+document.querySelector('.btn2').addEventListener('click', function(){
+  document.querySelector('.container').style.transform = 'translate(-100vw)';
+});
+document.querySelector('.btn3').addEventListener('click', function(){
+  document.querySelector('.container').style.transform = 'translate(-200vw)';
+});
+document.querySelector('.btn4').addEventListener('click', function(){
+  document.querySelector('.container').style.transform = 'translate(-300vw)';
+});
+document.querySelector('.btn5').addEventListener('click', function(){
+  document.querySelector('.container').style.transform = 'translate(-400vw)';
+});
