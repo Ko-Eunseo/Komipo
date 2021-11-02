d9c1d493e18b3f321fe73f515c276faed361fac4
github Ko-Eunseo <github koyk0408@naver.com>
2021-11-02 23:32:41 +0900

    Komipo layout

--- js/common.js
+++ js/common.js
@@ -10,18 +10,18 @@ function openNcloseNav(){
 }
 
 // button
-document.querySelector('.btn1').addEventListener('click', function(){
-  document.querySelector('.container').style.transform = 'translate(0)';
-});
-document.querySelector('.btn2').addEventListener('click', function(){
-  document.querySelector('.container').style.transform = 'translate(-100vw)';
-});
-document.querySelector('.btn3').addEventListener('click', function(){
-  document.querySelector('.container').style.transform = 'translate(-200vw)';
-});
-document.querySelector('.btn4').addEventListener('click', function(){
-  document.querySelector('.container').style.transform = 'translate(-300vw)';
-});
-document.querySelector('.btn5').addEventListener('click', function(){
-  document.querySelector('.container').style.transform = 'translate(-400vw)';
-});
+// document.querySelector('.btn1').addEventListener('click', function(){
+//   document.querySelector('.container').style.transform = 'translate(0)';
+// });
+// document.querySelector('.btn2').addEventListener('click', function(){
+//   document.querySelector('.container').style.transform = 'translate(-100vw)';
+// });
+// document.querySelector('.btn3').addEventListener('click', function(){
+//   document.querySelector('.container').style.transform = 'translate(-200vw)';
+// });
+// document.querySelector('.btn4').addEventListener('click', function(){
+//   document.querySelector('.container').style.transform = 'translate(-300vw)';
+// });
+// document.querySelector('.btn5').addEventListener('click', function(){
+//   document.querySelector('.container').style.transform = 'translate(-400vw)';
+// });
