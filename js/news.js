a1880e3ef86920f2ac7c16c9d328bf4db35aba74
github Ko-Eunseo <github koyk0408@naver.com>
2021-11-09 22:32:08 +0900

    Komipo news api

--- js/news.js
+++ js/news.js
@@ -1,11 +1,12 @@
+// 네이버 검색 API예제는 블로그를 비롯 전문자료까지 호출방법이 동일하므로 blog검색만 대표로 예제를 올렸습니다.
 // 네이버 검색 Open API 예제 - 블로그 검색
 var express = require('express');
 var app = express();
 var client_id = 'UGzGF_hY8oUbI_3nYm7E';
-var client_secret = 'YUGzGF_hY8oUbI_3nYm7E';
-app.get('/search/news', function (req, res) {
-  var api_url = 'https://openapi.naver.com/v1/search/news?query=' + encodeURI(req.query.query); // json 결과
-  var request = require('한국중부발전');
+var client_secret = 'YmTrySyGBp';
+app.get('search/news', function (req, res) {
+  var api_url = 'https://openapi.naver.com/v1/search/news.json?query=한국중부발전' + encodeURI(req.query.query); // json 결과
+  var request = require('request');
   var options = {
       url: api_url,
       headers: {'X-Naver-Client-Id':client_id, 'X-Naver-Client-Secret': client_secret}
@@ -21,5 +22,5 @@ app.get('/search/news', function (req, res) {
   });
 });
 app.listen(3000, function () {
-  console.log('http://127.0.0.1:3000/search/blog?query=검색어 app listening on port 3000!');
+  console.log('http://127.0.0.1:3000/search/news?query=한국중부발전 app listening on port 3000!');
 });
